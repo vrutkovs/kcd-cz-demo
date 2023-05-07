4b7bd35c0ed16b496955b8f9cdd238ff22bb5ef9
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-05-07 10:10:03 +0200

    Update bucket script

--- create-s3-bucket.sh
+++ create-s3-bucket.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -eux
-BUCKET_NAME=vrutkovs-hypershift-demo-may7
+BUCKET_NAME=vrutkovs-hypershift-demo
 aws s3api create-bucket --bucket $BUCKET_NAME
 aws s3api delete-public-access-block --bucket $BUCKET_NAME
-echo '{
-    "Version": "2012-10-17",
-    "Statement": [
+echo "{
+    \"Version\": \"2012-10-17\",
+    \"Statement\": [
         {
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${BUCKET_NAME}/*"
+            \"Effect\": \"Allow\",
+            \"Principal\": \"*\",
+            \"Action\": \"s3:GetObject\",
+            \"Resource\": \"arn:aws:s3:::${BUCKET_NAME}/*\"
         }
     ]
-}' | envsubst > policy.json
-aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
+}" | envsubst > secrets/policy.json
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://secrets/policy.json
