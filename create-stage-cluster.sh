4026feae5813a34216b506e4a85fbe69f1f3ba7a
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-09-23 08:51:55 +0200

    Update initial cluster versions

--- create-stage-cluster.sh
+++ create-stage-cluster.sh
@@ -15,4 +15,4 @@ hypershift create cluster aws \
     --node-pool-replicas 1 \
     --namespace clusters \
 	--etcd-storage-class ssd-csi \
-    --release-image quay.io/openshift-release-dev/ocp-release:4.12.12-x86_64
+    --release-image quay.io/openshift-release-dev/ocp-release:4.13.13-x86_64
