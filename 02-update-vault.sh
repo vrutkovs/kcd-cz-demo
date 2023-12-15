03133a7fa5e8a802bd5166686d3851ff17818995
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-12-15 15:39:04 +0100

    Fill in vault with a script

---
+++ 02-update-vault.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eux
+oc project vault
+for file in $(ls secrets/); do
+  oc exec -ti vault-0 -- sh -c "cat - > /tmp/${file}" < secrets/${file}
+done
+oc exec -ti vault-0 -- bash -c "chmod a+x /tmp/setup-vault.sh && bash /tmp/setup-vault.sh"
+
+# Create a token for external secrets
+oc create secret generic external-secrets-token --from-literal=token=hvs.ueL61rH30S3NvnlBZP8cJtJV -n vault
