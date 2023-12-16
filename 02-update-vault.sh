e6d1f32ac1e8890fa4ec540ccf7feadcf200a34b
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-12-16 14:17:56 +0100

    Update vault svc

--- 02-update-vault.sh
+++ 02-update-vault.sh
@@ -7,4 +7,4 @@ done
 oc exec -ti vault-0 -- bash -c "chmod a+x /tmp/setup-vault.sh && bash /tmp/setup-vault.sh"
 
 # Create a token for external secrets
-oc create secret generic external-secrets-token --from-literal=token=hvs.CSJ3zVRCxUbclrFM3nIKFhVB -n vault
+oc create secret generic external-secrets-token --from-literal=token=hvs.V9Xif89OcJRrxY5HVamFzPwJ -n vault
