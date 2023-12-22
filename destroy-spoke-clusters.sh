59de634e90c0e9cd8ba546d237804f94c860e19b
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-12-22 12:42:12 +0100

    Create dev cluster on KubeVirt too

--- destroy-spoke-clusters.sh
+++ destroy-spoke-clusters.sh
@@ -1,33 +1,16 @@
 export AWS_CREDS=/var/home/vrutkovs/src/github.com/vrutkovs/okd-installer/.aws/credentials
-export REGION=eu-west-3
 export BASE_DOMAIN=devcluster.openshift.com
 hypershift destroy cluster aws \
     --name vrutkovs-prod-eu \
     --infra-id vrutkovs-prod-eu \
     --aws-creds $AWS_CREDS \
-    --region $REGION \
+    --region eu-west-3 \
     --base-domain $BASE_DOMAIN
 
-export REGION=us-west-2
+export REGION=
 hypershift destroy cluster aws \
     --name vrutkovs-prod-us \
     --infra-id vrutkovs-prod-us \
     --aws-creds $AWS_CREDS \
-    --region $REGION \
-    --base-domain $BASE_DOMAIN
-
-export REGION=us-east-2
-hypershift destroy cluster aws \
-    --name vrutkovs-stage \
-    --infra-id vrutkovs-stage \
-    --aws-creds $AWS_CREDS \
-    --region $REGION \
-    --base-domain $BASE_DOMAIN
-
-export REGION=us-east-2
-hypershift destroy cluster aws \
-    --name vrutkovs-dev \
-    --infra-id vrutkovs-dev \
-    --aws-creds $AWS_CREDS \
-    --region $REGION \
+    --region us-west-2 \
     --base-domain $BASE_DOMAIN
