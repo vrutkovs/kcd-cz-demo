31043f1dfb8e8ea82399169f75bb616983e0567e
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-03-05 13:08:37 +0100

    02-create-google-serviceaccount.sh: use custom yq path

--- 02-create-google-serviceaccount.sh
+++ 02-create-google-serviceaccount.sh
@@ -1,4 +1,4 @@
-set +x
+set +eux
 gcloud iam service-accounts create vrutkovs --description="vrutkovs" --display-name="vrutkovs"
 gcloud projects add-iam-policy-binding  openshift-gce-devel \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
@@ -31,4 +31,4 @@ gcloud iam service-accounts keys create \
   ~/src/github.com/vrutkovs/okd-installer/.gcp/credentials \
   --iam-account="vrutkovs@openshift-gce-devel.iam.gserviceaccount.com"
 cp -rvf ~/src/github.com/vrutkovs/okd-installer/.gcp/credentials secrets/google-creds.json
-yq e '.config.service_account=load_str("secrets/google-creds.json")' -i secrets/thanos-object-storage.yaml
+/home/vrutkovs/.local/bin/yq e '.config.service_account=load_str("secrets/google-creds.json")' -i secrets/thanos-object-storage.yaml
