b222a93f584c1b52f672c76e1fa2e5a15281cffe
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-05-03 12:19:31 +0200

    google sa: install gcloud, set condition

--- 02-create-google-serviceaccount.sh
+++ 02-create-google-serviceaccount.sh
@@ -1,33 +1,45 @@
 set +eux
+
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+sudo dnf install -y google-cloud-cli
+
 gcloud iam service-accounts create vrutkovs --description="vrutkovs" --display-name="vrutkovs"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/compute.admin"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/compute.loadBalancerAdmin"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/compute.networkAdmin"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/dns.admin"
-# gcloud projects add-iam-policy-binding openshift-gce-devel \
+# gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
 #   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
 #   --role="roles/iam.organizationRoleViewer"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/iam.serviceAccountAdmin"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/iam.securityAdmin"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/iam.serviceAccountKeyAdmin"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/iam.serviceAccountUser"
-gcloud projects add-iam-policy-binding openshift-gce-devel \
+gcloud projects add-iam-policy-binding openshift-gce-devel --condition=None \
   --member="serviceAccount:vrutkovs@openshift-gce-devel.iam.gserviceaccount.com" \
   --role="roles/storage.admin"
 gcloud iam service-accounts keys create \
