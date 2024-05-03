db915f106bd0f056466ec66b2a584f7136cc1503
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-05-03 17:23:45 +0200

    create s3 bucket: use new policy

--- 01-create-s3-bucket.sh
+++ 01-create-s3-bucket.sh
@@ -20,5 +20,16 @@ aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
 
 BUCKET_NAME=vrutkovs-demo
 aws s3api create-bucket --bucket $BUCKET_NAME
+echo "{
+    \"Version\": \"2012-10-17\",
+    \"Statement\": [
+        {
+            \"Effect\": \"Allow\",
+            \"Principal\": \"*\",
+            \"Action\": \"s3:GetObject\",
+            \"Resource\": \"arn:aws:s3:::${BUCKET_NAME}/*\"
+        }
+    ]
+}" | envsubst > policy.json
 aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
 rm -rf policy.json
