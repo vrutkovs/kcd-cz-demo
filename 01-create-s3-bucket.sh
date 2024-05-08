36fb9eb290c88ad0851f347038d77d51346d2125
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-05-08 15:22:00 +0200

    s3: ensure all objects are world readable

--- 01-create-s3-bucket.sh
+++ 01-create-s3-bucket.sh
@@ -3,8 +3,8 @@ set -eux
 sudo dnf install -y /usr/bin/aws
 
 BUCKET_NAME=vrutkovs-hypershift-demo
-aws s3api create-bucket --bucket $BUCKET_NAME
-aws s3api delete-public-access-block --bucket $BUCKET_NAME
+aws s3api create-bucket --bucket "${BUCKET_NAME}"
+aws s3api delete-public-access-block --bucket "${BUCKET_NAME}"
 echo "{
     \"Version\": \"2012-10-17\",
     \"Statement\": [
@@ -16,9 +16,25 @@ echo "{
         }
     ]
 }" | envsubst > policy.json
-aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
+aws s3api put-bucket-policy --bucket "${BUCKET_NAME}" --policy file://policy.json
 rm -rf policy.json
 
 BUCKET_NAME=vrutkovs-demo
 aws s3api create-bucket --bucket $BUCKET_NAME
-aws s3api delete-public-access-block --bucket $BUCKET_NAME
+aws s3api put-public-access-block --bucket "${BUCKET_NAME}" --public-access-block-configuration "BlockPublicPolicy=false" # 2
+aws s3api put-bucket-policy --bucket "${BUCKET_NAME}" --policy '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::'"${BUCKET_NAME}"'/*"
+            ]
+        }
+    ]
+}'
